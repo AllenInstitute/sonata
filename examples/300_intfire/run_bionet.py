4652a2268f8ff3e2b1a5a21f518aa0d2cffdfd64
kaeld <kaeld@alleninstitute.org>
2018-09-14 09:40:58 -0700

    updating config file and adding magic to spikes h5

--- examples/300_intfire/run_bionet.py
+++ examples/300_intfire/run_bionet.py
@@ -1,6 +1,5 @@
 import sys, os
 from bmtk.simulator import bionet
-from bmtk.analyzer.visualization.spikes import plot_spikes
 
 
 def run(config_file):
@@ -10,7 +9,6 @@ def run(config_file):
     sim = bionet.BioSimulator.from_config(conf, network=net)
     sim.run()
     bionet.nrn.quit_execution()
-    # plot_spikes('network/v1_nodes.h5', 'network/v1_node_types.csv', 'output/spikes.h5', group_key='pop_name')
 
 
 if __name__ == '__main__':
