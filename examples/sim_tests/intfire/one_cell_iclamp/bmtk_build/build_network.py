42b1336453f058be1928cfa821081193f2b52912
Padraig Gleeson <p.gleeson@gmail.com>
2018-11-20 10:34:50 +0000

    Initial test on intfire

---
+++ examples/sim_tests/intfire/one_cell_iclamp/bmtk_build/build_network.py
@@ -0,0 +1,28 @@
+#!/bin/env python
+
+# Sonata Simulator Test Suite
+# 1-cell circuit, integrate and fire cell, current clamp input
+
+# Authors: Padraig Gleeson @ UC
+
+import numpy as np
+from neuron import h
+from bmtk.builder import NetworkBuilder
+
+net = NetworkBuilder("one_cell_iclamp")
+
+pos_x, pos_y = [0,0]
+
+template = 'nest:iaf_psc_alpha'
+
+net.add_nodes(N=1, pop_name='LIF_exc', location='VisL4', ei='e',
+              model_type='point_process',  # use point_process to indicate were are using point model cells
+              model_template=template,
+              x=pos_x, y=pos_y,
+              dynamics_params='472363762_point.json')
+              
+
+net.build()
+net.save(output_dir='../input/network')
+
+print 'done'
