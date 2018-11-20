4c75c50b73db837f97b98b6b1cded5bf9888330d
Werner Van Geit <werner.vangeit@epfl.ch>
2018-11-20 09:14:39 +0100

    First version of sim test suite

---
+++ examples/sim_tests/biophysical/one_cell/soma_cm2/bmtk_test/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+rm -rf output
+rm -rf __pycache__
+# python -m trace --trace ../shared_components/scripts/run_bionet.py ../input/config.json
+python ../shared_components/scripts/run_bionet.py ../input/config.json
+
+pytest -vvs
---
+++ examples/sim_tests/biophysical/one_cell/soma_cm2_hh/bmtk_test/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf output
+rm -rf __pycache__
+# python -m trace --trace ../shared_components/scripts/run_bionet.py ../input/config.json
+python ../shared_components/scripts/run_bionet.py ../input/config.json
+
+pytest -vvs
