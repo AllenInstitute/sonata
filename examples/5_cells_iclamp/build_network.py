f82ab3ee27bb559d696dbba6989d42f1a7d52061
kael <you@example.com>
2019-01-29 18:40:21 -0800

    Fixing 5_cells_iclamp example to use NeuroML files

--- examples/5_cells_iclamp/build_network.py
+++ examples/5_cells_iclamp/build_network.py
@@ -5,28 +5,23 @@ from bmtk.builder import NetworkBuilder
 cell_models = [
     {
         'model_name': 'Scnn1a', 'x': [0.0], 'y': [0.0], 'z': [0.0], 'ei': 'e', 'morphology': 'Scnn1a_473845048_m',
-        'model_template': 'ctdb:Biophys1.hoc',
-        'dynamics_params': '472363762_fit.json',
+        'model_template': 'nml:Cell_472363762.cell.nml'
     },
     {
         'model_name': 'Rorb', 'x': [200.0], 'y': [0.0], 'z': [0.0], 'ei': 'e', 'morphology': 'Rorb_325404214_m',
-        'model_template': 'ctdb:Biophys1.hoc',
-        'dynamics_params': '473863510_fit.json',
+        'model_template': 'nml:Cell_473863510.cell.nml'
     },
     {
         'model_name': 'Nr5a1', 'x': [-200.0], 'y': [0.0], 'z': [0.0], 'ei': 'e', 'morphology': 'Nr5a1_471087815_m',
-        'model_template': 'ctdb:Biophys1.hoc',
-        'dynamics_params': '473863035_fit.json',
+        'model_template': 'nml:Cell_473863035.cell.nml'
     },
     {
         'model_name': 'PV1', 'x': [0.0], 'y': [200.0], 'z': [0.0], 'ei': 'i', 'morphology': 'Pvalb_470522102_m',
-        'model_template': 'ctdb:Biophys1.hoc',
-        'dynamics_params': '472912177_fit.json',
+        'model_template': 'nml:Cell_472912177.cell.nml'
     },
     {
         'model_name': 'PV2', 'x': [0.0], 'y': [-200.0], 'z': [0.0], 'ei': 'i', 'morphology': 'Pvalb_469628681_m',
-        'model_template': 'ctdb:Biophys1.hoc',
-        'dynamics_params': '473862421_fit.json',
+        'model_template': 'nml:Cell_473862421.cell.nml'
     }
 ]
 
